335bfd63f0278d181713270d0459ec17f283e211
Luigi <russo.1699981@studenti.uniroma1.it>
2019-11-29 11:59:29 +0100

    FiltersFragment: use view model, provider filtering, some cleanup

--- app/src/main/java/com/example/biblio/fragments/FiltersFragment.java
+++ app/src/main/java/com/example/biblio/fragments/FiltersFragment.java
@@ -9,18 +9,18 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.biblio.R;
+import com.example.biblio.viewmodels.SearchViewModel;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.checkbox.MaterialCheckBox;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import lrusso96.simplebiblio.core.Ebook;
+import lrusso96.simplebiblio.core.Provider;
+import lrusso96.simplebiblio.core.providers.feedbooks.Feedbooks;
+import lrusso96.simplebiblio.core.providers.libgen.LibraryGenesis;
 
 public class FiltersFragment extends Fragment {
     private MaterialButton mLowRatingBtn;
@@ -34,13 +34,13 @@ public class FiltersFragment extends Fragment {
     private MaterialCheckBox mFrenchCb;
     private MaterialCheckBox mSpanishCb;
     private MaterialCheckBox mGermanCb;
-    private List<Ebook> search_results;
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.filters_fragment, container, false);
+
+        SearchViewModel model = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(SearchViewModel.class);
 
         ImageView mBackBtn = view.findViewById(R.id.filters_back_btn);
         MaterialButton mResetBtn = view.findViewById(R.id.filters_reset_btn);
@@ -55,23 +55,17 @@ public class FiltersFragment extends Fragment {
         mFrenchCb = view.findViewById(R.id.french_cb);
         mSpanishCb = view.findViewById(R.id.spanish_cb);
         mGermanCb = view.findViewById(R.id.german_cb);
-        MaterialButton mApplyFiltersBtn = view.findViewById(R.id.apply_filters_btn);
 
-        search_results = new Gson().fromJson(Objects.requireNonNull(getArguments()).getString("search_data"), new TypeToken<ArrayList<Ebook>>() {
-        }.getType());
+        mBackBtn.setOnClickListener(x -> Objects.requireNonNull(getFragmentManager()).popBackStackImmediate());
 
-
-        mBackBtn.setOnClickListener(view1 -> Objects.requireNonNull(getFragmentManager()).popBackStackImmediate());
-
-
-        mLowRatingBtn.setOnClickListener(view12 -> {
+        mLowRatingBtn.setOnClickListener(x -> {
             mLowRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mMediumRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mMediumHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
         });
 
-        mMediumRatingBtn.setOnClickListener(view13 -> {
+        mMediumRatingBtn.setOnClickListener(x -> {
             mLowRatingBtn.setBackgroundColor(getResources().getColor(R.color.disabled_button));
             mMediumRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mMediumHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
@@ -79,42 +73,35 @@ public class FiltersFragment extends Fragment {
         });
 
 
-        mMediumHighRatingBtn.setOnClickListener(view14 -> {
+        mMediumHighRatingBtn.setOnClickListener(x -> {
             mLowRatingBtn.setBackgroundColor(getResources().getColor(R.color.disabled_button));
             mMediumRatingBtn.setBackgroundColor(getResources().getColor(R.color.disabled_button));
             mMediumHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
         });
 
-        mHighRatingBtn.setOnClickListener(view15 -> {
+        mHighRatingBtn.setOnClickListener(x -> {
             mLowRatingBtn.setBackgroundColor(getResources().getColor(R.color.disabled_button));
             mMediumRatingBtn.setBackgroundColor(getResources().getColor(R.color.disabled_button));
             mMediumHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.disabled_button));
             mHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
         });
 
+        mFeedbooksCb.setChecked(model.isEnabled(Provider.FEEDBOOKS));
+        mFeedbooksCb.setOnCheckedChangeListener((button, isChecked) -> {
+            model.enableProvider(Feedbooks.class, isChecked);
+        });
 
-        mLibgenesisCb.setOnClickListener(view16 -> {
-            mFeedbooksCb.setChecked(false);
+        mLibgenesisCb.setChecked(model.isEnabled(Provider.LIBGEN));
+        mLibgenesisCb.setOnCheckedChangeListener((button, isChecked) -> {
+            model.enableProvider(LibraryGenesis.class, isChecked);
+        });
+
+
+        mResetBtn.setOnClickListener(x -> {
             mLibgenesisCb.setChecked(true);
-        });
-
-        mFeedbooksCb.setOnClickListener(view17 -> {
-            //Add component dynamically
-            //LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            //TextView tv = new TextView(getContext());
-            //tv.setLayoutParams(lparams);
-            //tv.setText("Feedbook selected");
-
-            //mLinearLayout.addView(tv);
-
             mFeedbooksCb.setChecked(true);
-            mLibgenesisCb.setChecked(false);
-        });
 
-        mResetBtn.setOnClickListener(view18 -> {
-            mLibgenesisCb.setChecked(false);
-            mFeedbooksCb.setChecked(false);
             mLowRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mMediumRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
             mMediumHighRatingBtn.setBackgroundColor(getResources().getColor(R.color.add_button));
@@ -126,34 +113,6 @@ public class FiltersFragment extends Fragment {
             mGermanCb.setChecked(false);
         });
 
-        mApplyFiltersBtn.setOnClickListener(view19 -> {
-            String provider = mLibgenesisCb.isChecked() ? mLibgenesisCb.getText().toString() : mFeedbooksCb.getText().toString();
-            Double rating = 0.0;
-            ArrayList<String> languages = new ArrayList<>();
-
-            if (mItalianCb.isChecked()) languages.add("it");
-            if (mEnglishCb.isChecked()) languages.add("en");
-            if (mFrenchCb.isChecked()) languages.add("fr");
-            if (mSpanishCb.isChecked()) languages.add("es");
-            if (mGermanCb.isChecked()) languages.add("de");
-
-
-            //fixme: unused variable
-            List<Ebook> filtered_results = filterResults(provider, rating, languages);
-        });
-
         return view;
-    }
-
-    private List<Ebook> filterResults(String provider, Double rating, ArrayList<String> languages) {
-
-        List<Ebook> filtered_results = new ArrayList<>();
-
-        //todo: rating not managed yet
-        for (Ebook elem : search_results) {
-            if (elem.getProviderName().equals(provider) && languages.contains(elem.getLanguage()))
-                filtered_results.add(elem);
-        }
-        return filtered_results;
     }
 }

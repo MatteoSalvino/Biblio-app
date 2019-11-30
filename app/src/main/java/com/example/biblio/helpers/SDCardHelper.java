96d3e7a4bc00a6d1f372ffa9d4e16fc745ec0de5
Luigi <russo.1699981@studenti.uniroma1.it>
2019-11-30 10:38:45 +0100

    SDCardHelper: add static method to get filename

--- app/src/main/java/com/example/biblio/helpers/SDCardHelper.java
+++ app/src/main/java/com/example/biblio/helpers/SDCardHelper.java
@@ -2,7 +2,11 @@ package com.example.biblio.helpers;
 
 import android.os.Environment;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
+
+import lrusso96.simplebiblio.core.Ebook;
 
 public class SDCardHelper {
 
@@ -12,13 +16,10 @@ public class SDCardHelper {
         return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
     }
 
-    public static boolean findFile(File dir, String filename, boolean removeOption) {
+    public static boolean findFile(@NotNull File dir, String filename, boolean removeOption) {
         File[] children = dir.listFiles();
-
-        if(children != null) {
-
+        if (children != null) {
             for (File child : children) {
-                //Log.d("findFile", child.getName());
                 if (filename.equals(child.getName())) {
                     if (removeOption)
                         return child.delete();
@@ -28,4 +29,12 @@ public class SDCardHelper {
         return false;
     }
 
+    /**
+     * @param ebook (assume at least one available download)
+     * @return the filename
+     */
+    @NotNull
+    public static String getFilename(@NotNull Ebook ebook) {
+        return String.format("%s_%s_%s.%s", ebook.getTitle(), ebook.getAuthor(), ebook.getPublished(), ebook.getDownloads().get(0).getExtension());
+    }
 }

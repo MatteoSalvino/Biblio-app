51656671355b1b922af4cc18eee1c28240a11b36
Luigi <russo.1699981@studenti.uniroma1.it>
2019-12-04 11:08:07 +0100

    SharedPreferencesHelper: keys refactoring

--- app/src/main/java/com/example/biblio/helpers/SharedPreferencesHelper.java
+++ app/src/main/java/com/example/biblio/helpers/SharedPreferencesHelper.java
@@ -1,5 +1,6 @@
 package com.example.biblio.helpers;
 
 public class SharedPreferencesHelper {
-    public static final String MY_EBOOKS_TAG = "my_ebooks";
+    public static final String MY_EBOOKS_KEY = "my_ebooks";
+    public static final String CURRENT_USER_KEY = "current_user";
 }

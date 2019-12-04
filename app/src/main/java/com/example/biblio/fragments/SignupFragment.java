7b0fa31626b600eecb2fec5da7a9f0d66c8c6ec1
Luigi <russo.1699981@studenti.uniroma1.it>
2019-12-04 11:11:35 +0100

    SignupFragment: use api, improved error handling

--- app/src/main/java/com/example/biblio/fragments/SignupFragment.java
+++ app/src/main/java/com/example/biblio/fragments/SignupFragment.java
@@ -1,5 +1,6 @@
 package com.example.biblio.fragments;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,24 +14,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.biblio.R;
+import com.example.biblio.api.User;
+import com.example.biblio.api.UserBuilder;
 import com.example.biblio.databinding.SignupFragmentBinding;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.Gson;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
+import static com.example.biblio.helpers.SharedPreferencesHelper.CURRENT_USER_KEY;
 
 public class SignupFragment extends Fragment {
+    public static final String TITLE = "Signup";
+    private final String LOG_TAG = getClass().getName();
     private SignupFragmentBinding binding;
     private ProgressDialog progressDialog;
 
@@ -48,77 +44,31 @@ public class SignupFragment extends Fragment {
             if (isValidForm(name, email, password, password_confirmation)) {
                 progressDialog = ProgressDialog.show(getActivity(), "", "", true);
                 progressDialog.setContentView(R.layout.login_dialog_view);
-                signup(name, email, password, password_confirmation);
+                new Thread(() -> {
+                    User user = new UserBuilder().setEmail(email).setPassword(password).setUsername(name).build();
+                    boolean successful = user.signup();
+                    getActivity().runOnUiThread(() -> progressDialog.dismiss());
+                    if (successful) {
+                        Log.d(LOG_TAG, "successful signup");
+                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString(CURRENT_USER_KEY, new Gson().toJson(user));
+                        editor.apply();
+                        getActivity().setResult(Activity.RESULT_OK);
+                        getActivity().finish();
+                    } else
+                        Log.d(LOG_TAG, "signup failed");
+                }).start();
             } else {
-                Log.d("FormValidation", "The signup form is not valid.");
+                Log.d(LOG_TAG, "The signup form is not valid.");
             }
         });
-
         return binding.getRoot();
     }
 
     //todo: make more sophisticated
-    private boolean isValidForm(String name, String email, String password, String password_confirmation) {
+    private boolean isValidForm(@NotNull String name, String email, String password, String password_confirmation) {
         return !name.isEmpty() && !email.isEmpty() && !password.isEmpty() && !password_confirmation.isEmpty()
                 && (password.compareTo(password_confirmation) == 0) && binding.signupTermsCb.isChecked();
-    }
-
-    private void signup(String name, String email, String password, String password_confirmation) {
-        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
-        String URL = "http://10.0.3.2:3000/signup";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, response -> {
-            progressDialog.dismiss();
-            Log.d("onResponse", response);
-
-            JsonObject jsonObject = (JsonObject) JsonParser.parseString(response);
-            String auth_token = jsonObject.get("auth_token").getAsString();
-
-            try {
-                String credentials = new JSONObject()
-                        .put("email", email)
-                        .put("password", password)
-                        .put("auth_token", auth_token).toString();
-
-                Log.d("credentials", credentials);
-
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-
-                editor.putString("credentials", credentials);
-                editor.apply();
-
-                getActivity().setResult(200);
-                getActivity().finish();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }, error -> {
-            progressDialog.dismiss();
-            Log.d("onErrorResponse", "Something goes wrong !");
-            error.printStackTrace();
-        }) {
-            @Override
-            public String getBodyContentType() {
-                return "application/x-www-form-urlencoded; charset=UTF-8";
-            }
-
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> postParams = new HashMap<>();
-                postParams.put("name", name);
-                postParams.put("email", email);
-                postParams.put("password", password);
-                postParams.put("password_confirmation", password_confirmation);
-                Log.d("getParams", postParams.toString());
-                return postParams;
-            }
-
-            @Override
-            protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                return super.parseNetworkResponse(response);
-            }
-        };
-        requestQueue.add(stringRequest);
     }
 }

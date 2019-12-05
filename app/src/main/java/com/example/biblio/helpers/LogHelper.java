627779c6716c3b70ddee68c03c1dccea88be92d5
Luigi <russo.1699981@studenti.uniroma1.it>
2019-12-05 19:30:36 +0100

    LogHelper: simple log module

---
+++ app/src/main/java/com/example/biblio/helpers/LogHelper.java
@@ -0,0 +1,35 @@
+package com.example.biblio.helpers;
+
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class LogHelper {
+    private final String tag;
+
+    public LogHelper(@NotNull Class clazz) {
+        tag = clazz.getName();
+    }
+
+    public void d(@Nullable String msg) {
+        Log.d(tag, lazy_null(msg));
+    }
+
+    public void e(@Nullable String msg) {
+        Log.e(tag, lazy_null(msg));
+    }
+
+    public void v(@Nullable String msg) {
+        Log.v(tag, lazy_null(msg));
+    }
+
+    public void w(@Nullable String msg) {
+        Log.w(tag, lazy_null(msg));
+    }
+
+    @NotNull
+    private String lazy_null(@Nullable String msg) {
+        return msg!=null? msg : "";
+    }
+}

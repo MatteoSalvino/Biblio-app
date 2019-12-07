b8f4f8ada5f56d15c3d0f44b5629fb70201c8e4e
Luigi <russo.1699981@studenti.uniroma1.it>
2019-12-07 15:45:02 +0100

    EbookDetailsViewModel: new view model for ebook details

---
+++ app/src/main/java/com/example/biblio/viewmodels/EbookDetailsViewModel.java
@@ -0,0 +1,30 @@
+package com.example.biblio.viewmodels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.biblio.helpers.LogHelper;
+
+import org.jetbrains.annotations.NotNull;
+
+import lrusso96.simplebiblio.core.Ebook;
+
+public class EbookDetailsViewModel extends ViewModel {
+    private MutableLiveData<Ebook> ebook;
+    private final LogHelper logger = new LogHelper(getClass());
+
+    public EbookDetailsViewModel() {
+        if (ebook == null)
+            ebook = new MutableLiveData<>();
+    }
+
+    public LiveData<Ebook> getEbook() {
+        return ebook;
+    }
+
+    public void setEbook(@NotNull Ebook ebook) {
+        logger.d(String.format("posting new ebook: %s", ebook.getTitle()));
+        this.ebook.postValue(ebook);
+    }
+}

cbca1e2011be6751b72225637683f1be8ce34da8
Luigi <russo.1699981@studenti.uniroma1.it>
2020-01-21 16:06:06 +0100

    SwipeEbooksFragment: enable providers (#11)

--- app/src/main/java/com/example/biblio/viewmodels/PopularViewModel.java
+++ app/src/main/java/com/example/biblio/viewmodels/PopularViewModel.java
@@ -1,11 +1,19 @@
 package com.example.biblio.viewmodels;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
 import java.util.List;
 
 import lrusso96.simplebiblio.core.Ebook;
 import lrusso96.simplebiblio.core.SimpleBiblio;
 
 public class PopularViewModel extends SwipeEbooksViewModel {
+
+    public PopularViewModel(@NonNull Application application) {
+        super(application);
+    }
 
     @Override
     protected List<Ebook> doRefresh(SimpleBiblio sb) {

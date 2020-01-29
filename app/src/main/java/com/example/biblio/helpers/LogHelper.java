6a19832cee85526c59da129850705b252b4d54a9
Luigi <russo.1699981@studenti.uniroma1.it>
2020-01-29 17:30:24 +0100

    LohHelper: new methods

--- app/src/main/java/com/example/biblio/helpers/LogHelper.java
+++ app/src/main/java/com/example/biblio/helpers/LogHelper.java
@@ -16,16 +16,32 @@ public class LogHelper {
         Log.d(tag, lazy_null(msg));
     }
 
+    public void d(@Nullable String msg, Throwable e) {
+        Log.d(tag, lazy_null(msg), e);
+    }
+
     public void e(@Nullable String msg) {
         Log.e(tag, lazy_null(msg));
+    }
+
+    public void e(@Nullable String msg, Throwable e) {
+        Log.e(tag, lazy_null(msg), e);
     }
 
     public void v(@Nullable String msg) {
         Log.v(tag, lazy_null(msg));
     }
 
+    public void v(@Nullable String msg, Throwable e) {
+        Log.v(tag, lazy_null(msg), e);
+    }
+
     public void w(@Nullable String msg) {
         Log.w(tag, lazy_null(msg));
+    }
+
+    public void w(@Nullable String msg, Throwable e) {
+        Log.w(tag, lazy_null(msg), e);
     }
 
     @NotNull

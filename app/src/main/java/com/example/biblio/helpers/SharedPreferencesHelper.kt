a3561c9b9b8210c231bc842b05dd26b958e76d8a
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-09 19:35:55 +0100

    SharedPreferencesHelper: migrate to ktx

---
+++ app/src/main/java/com/example/biblio/helpers/SharedPreferencesHelper.kt
@@ -0,0 +1,18 @@
+package com.example.biblio.helpers
+
+/**
+ * This class provides, as constants, a set of keys to access SharedPreferences.
+ */
+object SharedPreferencesHelper {
+    const val MY_EBOOKS_KEY = "my_ebooks"
+    const val CURRENT_USER_KEY = "current_user"
+    const val FIRST_START_KEY = "first_start"
+    const val LAST_SEARCH_TS_KEY = "last_search_ts"
+    const val LIBGEN_ENABLED_KEY = "libgen_enabled"
+    const val LIBGEN_MIRROR_KEY = "libgen_mirror"
+    const val LIBGEN_OVERRIDE_KEY = "libgen_override"
+    const val LIBGEN_MAX_RESULTS_KEY = "libgen_max"
+    const val FEEDBOOKS_ENABLED_KEY = "feedbooks_enabled"
+    const val STANDARD_EBOOKS_ENABLED_KEY = "standard_ebooks_enabled"
+    const val EAN_ENABLED_KEY = "ean_enabled"
+}

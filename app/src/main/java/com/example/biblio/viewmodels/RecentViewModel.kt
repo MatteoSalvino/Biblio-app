3b07fe64b30b751cfd5a46fb33d2f03ee63c0554
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-09 12:14:29 +0100

    RecentViewModel: migrate to ktx

---
+++ app/src/main/java/com/example/biblio/viewmodels/RecentViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.biblio.viewmodels
+
+import android.app.Application
+import lrusso96.simplebiblio.core.Ebook
+import lrusso96.simplebiblio.core.SimpleBiblio
+
+class RecentViewModel(application: Application) : SwipeEbooksViewModel(application) {
+    override suspend fun doRefresh(sb: SimpleBiblio): List<Ebook> = sb.allRecent
+}

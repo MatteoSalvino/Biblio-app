86516fdf8cc34d0767f4ec835a9e195a40f0e8bc
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-09 12:16:44 +0100

    RecentFragment: migrate to ktx

---
+++ app/src/main/java/com/example/biblio/fragments/RecentFragment.kt
@@ -0,0 +1,5 @@
+package com.example.biblio.fragments
+
+import com.example.biblio.viewmodels.RecentViewModel
+
+class RecentFragment : SwipeEbooksFragment(RecentFragment::class.java, RecentViewModel::class.java)

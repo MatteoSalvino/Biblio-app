545982721e41a02ed96c72968874b533db7bd593
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-09 12:16:24 +0100

    PopularFragment: migrate to ktx

---
+++ app/src/main/java/com/example/biblio/fragments/PopularFragment.kt
@@ -0,0 +1,5 @@
+package com.example.biblio.fragments
+
+import com.example.biblio.viewmodels.PopularViewModel
+
+class PopularFragment : SwipeEbooksFragment(PopularFragment::class.java, PopularViewModel::class.java)

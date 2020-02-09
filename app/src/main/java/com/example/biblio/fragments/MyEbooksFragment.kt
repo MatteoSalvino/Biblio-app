cb5599d3872b05209befb596c1c42e7f2a505c92
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-09 19:32:27 +0100

    MyEbooksFragment: migrate to ktx

---
+++ app/src/main/java/com/example/biblio/fragments/MyEbooksFragment.kt
@@ -0,0 +1,79 @@
+package com.example.biblio.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.FileProvider
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.biblio.BuildConfig
+import com.example.biblio.adapters.MyEbooksAdapter
+import com.example.biblio.databinding.FragmentMyEbooksBinding
+import com.example.biblio.helpers.SDCardHelper
+import com.example.biblio.helpers.SDCardHelper.getFilename
+import com.example.biblio.helpers.SimpleBiblioHelper.getMyEbooks
+import com.example.biblio.helpers.XFragment
+import com.example.biblio.viewmodels.EbookDetailsViewModel
+import lrusso96.simplebiblio.core.Ebook
+import org.apache.commons.io.FilenameUtils
+import java.io.File
+import java.util.*
+
+class MyEbooksFragment : XFragment(MyEbooksFragment::class.java), MyEbooksAdapter.OnItemListener {
+    private lateinit var mEbooks: ArrayList<Ebook>
+    private lateinit var binding: FragmentMyEbooksBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentMyEbooksBinding.inflate(inflater, container, false)
+        val mLayoutManager = LinearLayoutManager(context)
+        binding.myEbooksRv.layoutManager = mLayoutManager
+        binding.myEbooksRv.setHasFixedSize(true)
+        mEbooks = getMyEbooks(context!!)
+        logger.d(mEbooks.toString())
+        hideTemplates(mEbooks.isEmpty())
+        val mMyEbooksListener: MyEbooksAdapter.OnItemListener = this
+        val mAdapter = MyEbooksAdapter(mEbooks, mMyEbooksListener, context!!)
+        binding.myEbooksRv.adapter = mAdapter
+        return binding.root
+    }
+
+    /**
+     * Launches an ebook reader to open the file.
+     *
+     * @param filename name of the file, already downloaded, with extension (e.g. file.txt)
+     */
+    private fun openFile(filename: String) {
+        val path = File("${context?.getExternalFilesDir(null)?.absolutePath}/${SDCardHelper.APP_ROOT_DIR}/$filename")
+        logger.d("open file: $path")
+        val intent = Intent(Intent.ACTION_VIEW)
+        val uri = FileProvider.getUriForFile(context!!,
+                BuildConfig.APPLICATION_ID + ".provider",
+                path)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        //todo: what about other extensions supported by Mu?
+        when (FilenameUtils.getExtension(filename)) {
+            "epub" -> intent.setDataAndType(uri, "application/epub+zip")
+            "pdf" -> intent.setDataAndType(uri, "application/pdf")
+        }
+        startActivity(intent)
+    }
+
+    override fun onReadButtonClick(position: Int) {
+        val current = mEbooks[position]
+        openFile(getFilename(current))
+    }
+
+    private fun hideTemplates(should_hide: Boolean) {
+        val visibility = if (should_hide) View.VISIBLE else View.INVISIBLE
+        binding.ivTemplate.visibility = visibility
+        binding.tvTemplate.visibility = visibility
+    }
+
+    override fun onItemClick(position: Int) {
+        val model = ViewModelProvider(activity!!).get(EbookDetailsViewModel::class.java)
+        model.updateEbook(mEbooks[position])
+        moveTo(EbookDetailsFragment())
+    }
+}

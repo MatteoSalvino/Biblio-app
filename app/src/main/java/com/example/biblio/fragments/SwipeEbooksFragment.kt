94065f84c0deee1a2beaeec64f6c3c1a7079140c
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-09 19:34:50 +0100

    SwipeEbooksFragment: migrate to ktx

---
+++ app/src/main/java/com/example/biblio/fragments/SwipeEbooksFragment.kt
@@ -0,0 +1,49 @@
+package com.example.biblio.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.biblio.adapters.EbooksAdapter
+import com.example.biblio.databinding.FragmentSwipeEbooksBinding
+import com.example.biblio.helpers.XFragment
+import com.example.biblio.viewmodels.EbookDetailsViewModel
+import com.example.biblio.viewmodels.SwipeEbooksViewModel
+import lrusso96.simplebiblio.core.Ebook
+import java.util.*
+
+open class SwipeEbooksFragment internal constructor(clazz: Class<*>?, private val mSwipeModel: Class<out SwipeEbooksViewModel>) : XFragment(clazz!!), EbooksAdapter.OnItemListener {
+    private lateinit var mEbooksListener: EbooksAdapter.OnItemListener
+    private lateinit var mEbooks: ArrayList<Ebook>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentSwipeEbooksBinding.inflate(inflater, container, false)
+        val mLayoutManager = LinearLayoutManager(context)
+        binding.ebooksRv.layoutManager = mLayoutManager
+        binding.ebooksRv.setHasFixedSize(true)
+        //binding.swipeContainer.setColorSchemeResources(android.R.color.);
+        mEbooksListener = this
+        val model = ViewModelProvider(activity!!)[mSwipeModel]
+        binding.swipeContainer.setOnRefreshListener { model.refreshData() }
+        binding.swipeContainer.isRefreshing = true
+        val swipeObserver = Observer<List<Ebook>> { ebooks: List<Ebook> ->
+            binding.swipeContainer.isRefreshing = true
+            logger.d("swiping")
+            mEbooks = ebooks as ArrayList<Ebook>
+            val mAdapter = EbooksAdapter(mEbooks, mEbooksListener, context!!)
+            binding.ebooksRv.adapter = mAdapter
+            binding.swipeContainer.isRefreshing = false
+        }
+        model.getEbooks().observe(viewLifecycleOwner, swipeObserver)
+        return binding.root
+    }
+
+    override fun onItemClick(position: Int) {
+        val model = ViewModelProvider(activity!!).get(EbookDetailsViewModel::class.java)
+        model.updateEbook(mEbooks[position])
+        moveTo(EbookDetailsFragment())
+    }
+}

31f686facc3d93224883d99456e9fc847fab970d
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-10 12:15:50 +0100

    LoginActivity: use coroutines

--- app/src/main/java/com/example/biblio/LoginActivity.kt
+++ app/src/main/java/com/example/biblio/LoginActivity.kt
@@ -6,22 +6,22 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import com.example.biblio.api.User
 import com.example.biblio.api.UserBuilder
 import com.example.biblio.databinding.ActivityLoginBinding
 import com.example.biblio.helpers.GoogleHelper.getSignInIntent
-import com.example.biblio.helpers.LogHelper
 import com.example.biblio.helpers.SimpleBiblioHelper
+import com.example.biblio.helpers.XActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.apache.commons.validator.routines.EmailValidator
 
-class LoginActivity : AppCompatActivity() {
-    private val logger = LogHelper(javaClass)
-
+class LoginActivity : XActivity(LoginActivity::class.java) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -29,7 +29,8 @@ class LoginActivity : AppCompatActivity() {
         binding.loginBtn.setOnClickListener { _: View? ->
             val email = binding.emailField.editText!!.text.toString().trim { it <= ' ' }
             val password = binding.passwordField.editText!!.text.toString().trim { it <= ' ' }
-            if (validateEmail(email)) executeLogin(UserBuilder().setEmail(email).setPassword(password).build(), true)
+            if (validateEmail(email))
+                uiScope.launch { executeLogin(UserBuilder().setEmail(email).setPassword(password).build(), true) }
         }
         binding.googleLoginBtn.setOnClickListener { startActivityForResult(getSignInIntent(this), RC_GOOGLE_SIGN_IN) }
         binding.signupSuggestionBtn.setOnClickListener { startActivityForResult(Intent(this, SignupActivity::class.java), RC_SIGN_UP) }
@@ -62,7 +63,7 @@ class LoginActivity : AppCompatActivity() {
             val account = completedTask.getResult(ApiException::class.java)
             if (account != null) {
                 val user = UserBuilder().fromGoogleAccount(account).build()
-                executeLogin(user, false)
+                uiScope.launch { executeLogin(user, false) }
                 return
             }
         } catch (e: ApiException) {
@@ -81,21 +82,19 @@ class LoginActivity : AppCompatActivity() {
         return false
     }
 
-    private fun executeLogin(user: User, showProgress: Boolean) {
+    private suspend fun executeLogin(user: User, showProgress: Boolean) {
         val dialog = initProgress(showProgress)
-        Thread(Runnable {
-            val successful = user.login()
-            if (showProgress) runOnUiThread { dialog?.dismiss() }
-            if (successful) {
-                logger.d("successful login")
-                SimpleBiblioHelper.setCurrentUser(user, applicationContext)
-                setResult(Activity.RESULT_OK)
-                finish()
-            } else {
-                logger.d("login failed")
-                runOnUiThread { showErrorMessage() }
-            }
-        }).start()
+        val successful = withContext(Dispatchers.IO) { user.login() }
+        if (showProgress) dialog?.dismiss()
+        if (successful) {
+            logger.d("successful login")
+            SimpleBiblioHelper.setCurrentUser(user, applicationContext)
+            setResult(Activity.RESULT_OK)
+            finish()
+        } else {
+            logger.d("login failed")
+            showErrorMessage()
+        }
     }
 
     private fun initProgress(show: Boolean): ProgressDialog? {
